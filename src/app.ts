2d3ccb73558b2a9b3f748bdaafc95891edd86a13
Raineriojr <raijunior20@gmail.com>
2026-03-27 17:16:19 -0300

    feat: error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import fastify from "fastify";
+import z, { ZodError } from "zod";
+
 import { appRoutes } from "./http/routes";
+import { env } from "../env.schema";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error", issues: z.treeifyError(error) });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // implements monitoring
+  }
+
+  return reply.status(500).send({ message: "Internal server error" });
+});
