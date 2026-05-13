e107ce4e21baad346a92c70b77eeecd208dad5a2
Raineriojr <raijunior20@gmail.com>
2026-05-12 21:15:38 -0300

    chore: config tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,28 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
+  test: {
+    dir: "src",
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          dir: "src/use-cases",
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "e2e",
+          dir: "src/http/controllers",
+          environment: "./prisma/vitest-environment-prisma.ts",
+          sequence: {
+            concurrent: false,
+          },
+          fileParallelism: false,
+        },
+      },
+    ],
+  },
 });
