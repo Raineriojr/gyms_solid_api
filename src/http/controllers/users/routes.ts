bab7725d1179f98f87959c44e59892df5999c363
Raineriojr <raijunior20@gmail.com>
2026-05-12 22:05:20 -0300

    refactor: adjust directories

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,13 @@
+import type { FastifyInstance } from "fastify";
+import { registerController } from "./register.controller";
+import { authenticateController } from "./authenticate.controller";
+import { profileController } from "./profile.controller";
+import { verifyJWt } from "../../middlewares/verify-jwt";
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post("/users", registerController);
+  app.post("/sessions", authenticateController);
+
+  /** Authenticated */
+  app.get("/me", { onRequest: [verifyJWt] }, profileController);
+}
