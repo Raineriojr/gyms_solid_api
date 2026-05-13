5c17494e34f296c4efaaaac62ab2b38eca57ccb2
Raineriojr <raijunior20@gmail.com>
2026-05-13 10:36:53 -0300

    fix: function return

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -2,6 +2,6 @@ import type { Prisma, User } from "../../generated/prisma/client";
 
 export interface IUsersRepository {
   findByEmail(email: string): Promise<User | null>;
-  findById(id: string): Promise<User | null>;
+  findById(id: string): Promise<Partial<User> | null>;
   create(data: Prisma.UserCreateInput): Promise<User>;
 }
