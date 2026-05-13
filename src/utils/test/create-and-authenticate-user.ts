f4769e215a95397d2158a51a0ec095ab4c351766
Raineriojr <raijunior20@gmail.com>
2026-05-13 11:54:12 -0300

    fix: validate user role

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcrypt";
 import type { FastifyInstance } from "fastify";
 import request from "supertest";
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post("/users").send({
-    name: "user test",
-    email: "teste@email.com",
-    password: "123456",
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin: boolean = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: "user test",
+      email: "teste@email.com",
+      password_hash: await hash("123456", 6),
+      role: isAdmin ? "ADMIN" : "MEMBER",
+    },
   });
 
   const authResponse = await request(app.server).post("/sessions").send({
