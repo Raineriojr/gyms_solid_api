4bd884a421aa84e8ec64d49247b2c0d99c2466ad
Raineriojr <raijunior20@gmail.com>
2026-05-13 10:36:22 -0300

    feat: refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import z, { ZodError } from "zod";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 
 import { env } from "../env.schema";
 
@@ -12,7 +13,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 });
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { registerController } from "./register.controller";
 import { authenticateController } from "./authenticate.controller";
 import { profileController } from "./profile.controller";
 import { verifyJWt } from "../../middlewares/verify-jwt";
+import { refreshController } from "./refresh.controller";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
   app.post("/sessions", authenticateController);
 
+  app.patch("/token/refresh", refreshController);
   /** Authenticated */
   app.get("/me", { onRequest: [verifyJWt] }, profileController);
 }
