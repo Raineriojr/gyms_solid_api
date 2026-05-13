d71337efd4f623d346a2b49847bc656e6df16100
Raineriojr <raijunior20@gmail.com>
2026-05-12 22:03:37 -0300

    fix: rename function

--- src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
+++ src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -1,7 +1,7 @@
 import { FetchUsersCheckInsHistoryUseCase } from "../fetch-users-check-ins-history-use-case";
 import { PrismaCheckInsRepository } from "@/repositories/prisma/prisma-check-ins-repository";
 
-export const makeGetUserMetricsFetchUserCheckInsHistoryUseCase = () => {
+export const makeFetchUserCheckInsHistoryUseCase = () => {
   const usersRepository = new PrismaCheckInsRepository();
   const useCase = new FetchUsersCheckInsHistoryUseCase(usersRepository);
 
